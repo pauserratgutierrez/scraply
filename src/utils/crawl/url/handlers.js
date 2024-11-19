e08c4583900cb1d917c5cd884f4e47ba5eb19072
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2024-11-20 00:08:29 +0100

    Improve shouldRetry function
    - Add x-ratelimit-reset header
    - Add EXIT_ON_RATE_LIMIT toggle config option
    - Add CRAWL_RATE_LIMIT_FALLBACK_DELAY_MS config option
    - Add delay when rate limit

--- src/utils/crawl/url/handlers.js
+++ src/utils/crawl/url/handlers.js
@@ -1,19 +1,37 @@
 import { URL } from 'node:url';
 import { normalizeURL } from './normalize.js';
+import { delay } from '../delay.js';
 
 // Handle HTML Status Codes HERE!
-export const shouldRetry = (error) => {
+export const shouldRetry = async (error) => {
   if (!error.response) return true;
 
-  if (error.response.status === 429) {
-    const waitTime = error.response.headers?.['retry-after'];
-    if (waitTime) {
-      console.log(`Rate limited for ${waitTime} seconds, exiting...`);
+  const { status, headers } = error.response;
+  const retryAfter = headers?.['retry-after'];
+  const rateLimitReset = headers?.['x-ratelimit-reset'];
+
+  if (status === 429) {
+    let waitTime = null;
+
+    if (retryAfter) {
+      waitTime = isNan(retryAfter)
+        ? Math.ceil((new Date(retryAfter).getTime() - Date.now()) / 1000) // HTTP date
+        : parseInt(retryAfter, 10); // Seconds
+      console.log(`Rate limited. Retrying after ${waitTime} seconds...`);
+    } else if (rateLimitReset) {
+      waitTime = Math.max(parseInt(rateLimitReset, 10) - Math.floow(Date.now() / 1000), 0);
+      console.log(`Rate limited. Retrying after ${waitTime} seconds...`);
     } else {
-      console.log(`Rate limited, no retry-after header found, exiting...`);
+      console.log(`Rate limited. No 'retry-after' or 'x-ratelimit-reset' headers found.`);
+      waitTime = CONFIG.CRAWLER.CRAWL_RATE_LIMIT_FALLBACK_DELAY_MS / 1000;
     }
 
-    process.exit(CONFIG.CRAWLER.EXIT_CODE_RATE_LIMIT); // GitHub Actions Docker uses values ranged from 0 to 255, so any bigger value will be % 256!
+    if (CONFIG.CRAWLER.EXIT_ON_RATE_LIMIT) {
+      console.log(`Exiting due to rate limit.`);
+      process.exit(CONFIG.CRAWLER.EXIT_CODE_RATE_LIMIT); // GitHub Actions Docker uses values ranged from 0 to 255, so any bigger value will be % 256!
+    } else {
+      await delay(waitTime * 1000);
+    }
   }
 
   return CONFIG.CRAWLER.RETRY_STATUS_CODES.includes(error.response.status); // Retry only on specific status codes
