763a9844752cb5a24d6392bc0ddf9276cab3515f
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2024-11-19 19:44:36 +0100

    Remove default Hardcoded links config from defaultConfig.js

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -50,17 +50,6 @@ export const DEFAULT_CONFIG = {
         'mobile': 'mobile.json',
         '*': 'general.json'
       },
-    },
-    HARD_CODED_LINKS: [
-      // {
-      //   file_name: 'hc-links.json',
-      //   data: [
-      //     {
-      //       "url": "https://custom-link.com",
-      //       "content": "That's a custom link content, you can add as many as you want."
-      //     },
-      //   ]
-      // }
-    ]
+    }
   }
 };
