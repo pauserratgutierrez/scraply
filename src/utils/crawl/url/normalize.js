2fcf825181db757d78e4f94feaf4490bcd57e519
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2025-02-10 19:10:31 +0100

    Normalize URLs by forcing HTTPS and removing 'www.' prefix

--- src/utils/crawl/url/normalize.js
+++ src/utils/crawl/url/normalize.js
@@ -2,7 +2,13 @@ export const normalizeURL = (url) => {
   const urlObj = new URL(url);
   urlObj.hash = ''; // Remove the fragment part
   urlObj.search = ''; // Remove the query part
-  urlObj.pathname = urlObj.pathname.endsWith('/') ? urlObj.pathname.slice(0, -1) : urlObj.pathname; // Remove trailing slashes
-  urlObj.pathname = urlObj.pathname === '' ? '/' : urlObj.pathname; // Handle the root URL separately
+  urlObj.protocol = 'https:'; // Force HTTPS
+  urlObj.pathname = urlObj.pathname.endsWith('/')
+    ? urlObj.pathname.slice(0, -1)
+    : urlObj.pathname; // Remove trailing slashes
+  urlObj.pathname = urlObj.pathname === ''
+    ? '/'
+    : urlObj.pathname; // Handle the root URL separately
+  urlObj.hostname = urlObj.hostname.replace(/^www\./, ''); // Remove 'www.' prefix
   return urlObj.toString();
 };
